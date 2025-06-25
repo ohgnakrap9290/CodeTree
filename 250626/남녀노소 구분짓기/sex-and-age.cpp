3a68c19f5d8313ae85827ec283985d4962f8580c
ohgnakrap9290 <phantom4862@naver.com>
2025-06-26 02:01:28 +0900

    Title: 남녀노소 구분짓기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250626/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int gender,age;
+    cin >> gender >>age;
+    if(gender ==0){
+        if(age>=19){
+            cout <<"MAN";
+        }
+        else{
+            cout <<"BOY";
+        }
+    }
+    else{
+        if(age>=19){
+            cout <<"WOMAN";
+        }
+        else{
+            cout <<"GIRL";
+        }
+    }
+}
