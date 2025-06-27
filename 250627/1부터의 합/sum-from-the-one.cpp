d495f60751157a2de1e2bb4333db743141ffa55a
ohgnakrap9290 <phantom4862@naver.com>
2025-06-27 16:52:55 +0900

    Title: 1부터의 합, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250627/1부터의 합/sum-from-the-one.cpp
+++ 250627/1부터의 합/sum-from-the-one.cpp
@@ -1,19 +1,16 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int n,total=0;
-    cin >>n;
-    for(int i=1;i<100;i++){
-        if(total>n){
-            cout <<i-1;
+
+int main() {
+    int n, total = 0;
+    cin >> n;
+
+    for (int i = 1; i < 100; i++) {
+        total += i;
+
+        if (total >= n) {
+            cout << i;
             break;
-        }
-        else if(total ==n){
-            cout<<i;
-            break;
-        }
-        else{
-            total +=i;
         }
     }
 }
