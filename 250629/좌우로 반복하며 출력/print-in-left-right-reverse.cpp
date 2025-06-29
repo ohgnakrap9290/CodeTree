95976fb9f3d4857b03a591365617f92773f546ad
ohgnakrap9290 <phantom4862@naver.com>
2025-06-29 22:39:51 +0900

    Title: 좌우로 반복하며 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250629/좌우로 반복하며 출력/print-in-left-right-reverse.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >>n;
+    for(int i=0;i<n;i++){
+        if(i%2==0){
+            for(int j=1;j<=n;j++){
+                cout<<j;
+            }
+        }
+        else{
+            for(int k=n;k>0;k--){
+                cout << k;
+            }
+        }
+        cout <<'\n';
+    }
+}
