044c1d4871c90f884fc3f481ad84c73ce13b3d44
ohgnakrap9290 <phantom4862@naver.com>
2025-07-03 07:45:55 +0900

    Title: 날짜 변경하여 출력 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250703/날짜 변경하여 출력 2/print-date-with-different-format-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char _;
+    int a,b,c;
+    cin >>a>>_>>b>>_>>c;
+    cout<<c<<'.'<<a<<'.'<<b;
+    return 0;
+}
