a60f2f4421f94934ae9113091fbf61054195c857
ohgnakrap9290 <phantom4862@naver.com>
2025-07-03 07:54:38 +0900

    Title: 전화번호 바꾸기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250703/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    char _;
+    cin>>a>>_>>b>>_>>c;
+    cout<<"010"<<_<<c<<_<<b;
+    // Please write your code here.
+    return 0;
+}
