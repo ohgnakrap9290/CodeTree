f216ebc5f5312d5d8a151eedfc58dda205caf62e
ohgnakrap9290 <phantom4862@naver.com>
2025-07-04 07:23:01 +0900

    Title: 합과 평균, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250704/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin >>a>>b;
+    cout<<fixed;
+    cout.precision(1);
+    cout<< a+b<<' '<<(a+b)/2.0;
+    return 0;
+}
