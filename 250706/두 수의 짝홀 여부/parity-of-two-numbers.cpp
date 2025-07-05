143028d8d52e7e177e7aa81977656cbcf1c25990
ohgnakrap9290 <phantom4862@naver.com>
2025-07-06 06:01:31 +0900

    Title: 두 수의 짝홀 여부, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250706/두 수의 짝홀 여부/parity-of-two-numbers.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    cout<< ((a%2==0) ? "even":"odd")<<endl<<((b%2==0) ? "even":"odd");
+    return 0;
+}
