705eee68f0651b9859aacb4b35e33a3020adf562
ohgnakrap9290 <phantom4862@naver.com>
2025-07-06 06:39:18 +0900

    Title: 세 정수의 최솟값, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250706/세 정수의 최솟값/minimum-of-three-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin >>a>>b>>c;
+    int min1=min({a,b,c});
+    cout<<min1;
+
+    return 0;
+}
