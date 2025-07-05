6c43e203414ded4af79f25708a1bd25fb7613525
ohgnakrap9290 <phantom4862@naver.com>
2025-07-06 06:49:11 +0900

    Title: 정수의 조건 여부 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250706/정수의 조건 여부 3/numbers-condition-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin>>a;
+    cout<<((a%13==0||a%19==0)?"True":"False");
+    return 0;
+}
