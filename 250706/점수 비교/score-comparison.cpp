0b3bb5240324433437c116d8c1a8b1ec770705d7
ohgnakrap9290 <phantom4862@naver.com>
2025-07-06 06:17:31 +0900

    Title: 점수 비교, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250706/점수 비교/score-comparison.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int am,ae,bm,be;
+    cin>>am>>ae>>bm>>be;
+    cout<<(am>bm&&ae>be);
+    return 0;
+}
