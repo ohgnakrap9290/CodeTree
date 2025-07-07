9edf5d6b267bc4a76ccee3731cc9c0266feb6a7f
ohgnakrap9290 <phantom4862@naver.com>
2025-07-07 23:54:54 +0900

    Title: 좀 더 어려운 수학 점수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250707/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a1,a2,b1,b2;
+    cin >>a1>>a2>>b1>>b2;
+    if(a1==b1){
+        cout<<((a2>b2)?'A':'B');
+    }
+    else{
+        cout<<((a1>b1)?'A':'B');
+    }
+    return 0;
+}
