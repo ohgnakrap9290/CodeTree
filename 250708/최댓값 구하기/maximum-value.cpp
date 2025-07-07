c33334033146568b3f292b47b9c30d898c6938c5
ohgnakrap9290 <phantom4862@naver.com>
2025-07-08 00:13:14 +0900

    Title: 최댓값 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250708/최댓값 구하기/maximum-value.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include<algorithm>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c;
+    cin >>a>>b>>c;
+    cout<<max({a,b,c});
+    return 0;
+}
