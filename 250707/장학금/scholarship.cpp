8256e62ee9d4b9f9c315f7c4ee330bb77fc6e2a3
ohgnakrap9290 <phantom4862@naver.com>
2025-07-07 23:51:25 +0900

    Title: 장학금, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250707/장학금/scholarship.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    if(a>=90){
+        if(b>=95){
+            cout<<100000;
+        }
+        else if(b>=90){
+            cout<<50000;
+        }
+        else{
+            cout<<0;
+        }
+
+    }
+    else{
+        cout<<0;
+    }
+    return 0;
+}
