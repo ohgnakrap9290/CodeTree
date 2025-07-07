d6a68a85f8a1fbed093b0a91c17a12559f245551
ohgnakrap9290 <phantom4862@naver.com>
2025-07-07 23:41:32 +0900

    Title: 수의 계절은, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250707/수의 계절은/season-of-num.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int dnjf;
+    if(dnjf>=3&&dnjf<=5){
+        cout<<"Spring";
+    }
+    else if(dnjf>=6&&dnjf<=8){
+        cout<<"Summer";
+    }
+    else if(dnjf>=9&&dnjf<=11){
+        cout<<"Autumn";
+    }
+    else{
+        cout<<"Winter";
+    }
+    return 0;
+}
