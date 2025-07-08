e103768884fe8a1040f13936af1374ae4e422613
ohgnakrap9290 <phantom4862@naver.com>
2025-07-08 17:44:21 +0900

    Title: 별 N개 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250708/별 N개 출력하기/print-n-stars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int i=0,n;
+    cin>>n;
+    while(i<n){
+        cout<<'*'<<endl;
+        i++;
+    }
+    return 0;
+}
