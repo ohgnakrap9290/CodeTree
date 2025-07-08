273411117d063e2434f8ab95243ec831c39d0234
ohgnakrap9290 <phantom4862@naver.com>
2025-07-08 17:48:42 +0900

    Title: 3의 배수 출력하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250708/3의 배수 출력하기/print-multiple-of-number-three.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,a=1;
+    cin>>a;
+    while(a<=n){
+        cout<<a<<' ';
+        a*=3;
+    }
+    return 0;
+}
