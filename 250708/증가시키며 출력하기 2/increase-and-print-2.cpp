324c04b9927da5dc1e6df71b74573645453af1cf
ohgnakrap9290 <phantom4862@naver.com>
2025-07-08 17:39:30 +0900

    Title: 증가시키며 출력하기 2, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250708/증가시키며 출력하기 2/increase-and-print-2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int count=10;
+    while(count<=26){
+        cout<< count<<' ';
+        count +=1;
+    }
+    return 0;
+}
