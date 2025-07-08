b96d2f9cbf0364da0df70f813d9c24105e5daa3f
ohgnakrap9290 <phantom4862@naver.com>
2025-07-09 04:05:06 +0900

    Title: N번 더하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250709/N번 더하기/add-n-times.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,n;
+    cin >>a>>n;
+    for(int i=1;i<=n;i++){
+        cout<<a+n*i<<endl;
+    }
+    return 0;
+}
