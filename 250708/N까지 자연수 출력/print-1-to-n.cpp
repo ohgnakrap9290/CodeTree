6e306c9819735fcd5dfe20f9ad99d2c9c583068a
ohgnakrap9290 <phantom4862@naver.com>
2025-07-08 17:42:13 +0900

    Title: N까지 자연수 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250708/N까지 자연수 출력/print-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a=1,n;
+    cin>>n;
+    while(a<=n){
+        cout<<a<<' ';
+        a+=1;
+    }
+    return 0;
+}
