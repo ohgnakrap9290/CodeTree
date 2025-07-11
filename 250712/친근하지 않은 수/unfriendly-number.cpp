f94ddfab7175d52b244cb648a71b8617045b86e3
ohgnakrap9290 <phantom4862@naver.com>
2025-07-12 02:12:04 +0900

    Title: 친근하지 않은 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250712/친근하지 않은 수/unfriendly-number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,cnt=0;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        if((i%2==0)||(i%3==0)||(i%5==0)){
+            continue;
+        }
+        else{
+            cnt+=1;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
