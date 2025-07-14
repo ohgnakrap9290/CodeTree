edca216df92c7511a3725ca1ecf49278ddccd08c
ohgnakrap9290 <phantom4862@naver.com>
2025-07-14 20:40:31 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250714/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    for(;;){
+        cin>>n;
+        
+        if(n==0){
+            break;
+        }
+        cout<<n<<endl;
+    }
+    return 0;
+}
