cf2406523a11ee6ad44495b79eaf7009cd56765e
ohgnakrap9290 <phantom4862@naver.com>
2025-07-17 00:59:54 +0900

    Title: 별표 출력하기 6, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250717/별표 출력하기 6/print-star-6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int k=0;k<i;k++){
+            cout<<"  ";
+        }
+        for(int j=0;j<(4-i)*2-1;j++){
+            cout<<'*'<<' ';
+        }
+        cout<<endl;
+        
+    }
+    for(int i=n-2;i>=0;i--){
+        for(int k=0;k<i;k++){
+            cout<<"  ";
+        }
+        for(int j=0;j<(4-i)*2-1;j++){
+            cout<<'*'<<' ';
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+}
