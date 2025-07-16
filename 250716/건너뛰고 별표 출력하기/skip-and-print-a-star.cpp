21f4869f945470bfb9d054a3c581076d59c58a98
ohgnakrap9290 <phantom4862@naver.com>
2025-07-16 21:11:30 +0900

    Title: 건너뛰고 별표 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250716/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
+++ 250716/건너뛰고 별표 출력하기/skip-and-print-a-star.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main(){
     // Please write your code here.
     int n;
     cin>>n;
     for(int i=1;i<=n;i++){
         for(int j=0;j<i;j++){
-            cout<<'*'<<' ';
+            cout<<'*';
         }
         cout<<endl<<endl;
     }
     for(int i=n-1;i>0;i--){
         for(int j=0;j<i;j++){
-            cout<<'*'<<' ';
-        }
-        cout<<endl;
+            cout<<'*';
+        
     }
-    return 0;
-}
+    cout<<endl<<endl;
+}}
