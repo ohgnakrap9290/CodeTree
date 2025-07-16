7e601f5609da30ea6a6d7976f5d063f0c1561d3b
ohgnakrap9290 <phantom4862@naver.com>
2025-07-16 20:35:01 +0900

    Title: 모두 3의 배수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250716/모두 3의 배수/all-multiples-of-3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    bool yes=true;
+    for(int i=0;i<5;i++){
+        cin>>a;
+        if(a%3!=0){
+            yes=false;
+        }
+    }
+    if(yes){
+        cout<<1;
+
+    }
+    else{
+        cout<<0;
+    }
+    return 0;
+}
