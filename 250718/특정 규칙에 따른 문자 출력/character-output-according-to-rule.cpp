9d72adead9a59ea119984c06b1f1ae2343ca7931
ohgnakrap9290 <phantom4862@naver.com>
2025-07-18 00:20:13 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250718/특정 규칙에 따른 문자 출력/character-output-according-to-rule.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<' '<<' ';
+        }
+        for(int k=0;k<i;k++){
+            cout<<'@'<<' ';
+        }
+        cout<<endl;
+    }
+    for(int i=1;i<n;i++){
+        for(int k=0;k<n-i;k++){
+            cout<<'@'<<' ';
+        }
+        for(int j=0;j<i;j++){
+            cout<<' '<<' ';
+        }
+        
+        cout<<endl;
+    }
+    return 0;
+}
