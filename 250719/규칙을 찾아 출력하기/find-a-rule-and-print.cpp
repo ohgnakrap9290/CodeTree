3cedac78a78bb7ede4a291376d79338e35b7d3ee
ohgnakrap9290 <phantom4862@naver.com>
2025-07-19 04:25:28 +0900

    Title: 규칙을 찾아 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250719/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if((i==0)||(j==0)||(i==n-1)||(j==n-1)){
+                cout<<"* ";
+            }
+            else if(j<i){
+                cout<<"* ";
+            }
+            else{
+                cout<<"  ";
+            }
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
