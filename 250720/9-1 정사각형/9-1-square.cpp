9c0ab1fe7a8d03df6947f588b15eabc7090c003a
ohgnakrap9290 <phantom4862@naver.com>
2025-07-20 17:37:36 +0900

    Title: 9-1 정사각형, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250720/9-1 정사각형/9-1-square.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,cnt=9;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(cnt==0){
+                cnt=9;
+            }
+            cout<<cnt;
+            cnt-=1;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
