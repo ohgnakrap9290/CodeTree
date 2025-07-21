e79a3412c8a59fe349a7ace872c1fa9d6a967123
ohgnakrap9290 <phantom4862@naver.com>
2025-07-22 00:32:01 +0900

    Title: 숫자 피라미드 3, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250722/숫자 피라미드 3/number-pyramid-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=i;j<=(i*i);j+=i){
+            
+            
+            cout<<j<<' ';
+            
+        }
+        cout<<endl;
+    }
+    return 0;
+}
