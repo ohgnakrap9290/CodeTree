53b797056815de5a49c8579850aa90c49768b52c
ohgnakrap9290 <phantom4862@naver.com>
2025-07-26 18:42:38 +0900

    Title: 완전수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250726/완전수/perfect-number.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int start, end,cnt=0,total=0;
+
+int main() {
+    int start, end,cnt=0,total=0;
+    cin >> start >> end;
+
+    // Please write your code here.
+    for(int i=start;i<=end;i++){
+        for(int j=1;j<i;j++){
+            if(i%j==0){
+                total+=i;
+            }
+            if(total==i){
+            cnt+=1;
+        }
+        }
+        
+
+    }
+    cout<<cnt;
+
+    return 0;
+}
