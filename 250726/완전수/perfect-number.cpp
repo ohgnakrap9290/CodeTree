c8549c550425d06ad0a5f4ea0b398015f807a90c
ohgnakrap9290 <phantom4862@naver.com>
2025-07-26 18:47:09 +0900

    Title: 완전수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250726/완전수/perfect-number.cpp
+++ 250726/완전수/perfect-number.cpp
@@ -10,6 +10,7 @@ int main() {
 
     // Please write your code here.
     for(int i=start;i<=end;i++){
+        total=0;
         for(int j=1;j<i;j++){
             if(i%j==0){
                 total+=j;
