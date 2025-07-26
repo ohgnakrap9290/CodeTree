7349ebe350d62a46943ff8d9f84cecfa6c5913a7
ohgnakrap9290 <phantom4862@naver.com>
2025-07-26 19:09:59 +0900

    Title: 원소 10개의 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250726/원소 10개의 합/sum-of-10-elements.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int total=0,n;
+    for(int i=0;i<10;i++){
+        cin>>n;
+        total+=n;
+    }
+    cout<<total;
+    return 0;
+}
