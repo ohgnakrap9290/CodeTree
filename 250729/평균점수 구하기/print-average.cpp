cfc92aad7b905f8032b38a00d9ae9b71e9a138a0
ohgnakrap9290 <phantom4862@naver.com>
2025-07-29 15:33:48 +0900

    Title: 평균점수 구하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250729/평균점수 구하기/print-average.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double total=0,n;
+    for(int i=0;i<8;i++){
+        cin>>n;
+        total+=n;
+    }
+    cout<<total/8;
+    return 0;
+}
