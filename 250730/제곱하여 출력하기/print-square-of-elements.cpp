eaebf6d7dda6fe27d9fb65c56ac949868f5e6d92
ohgnakrap9290 <phantom4862@naver.com>
2025-07-30 02:05:01 +0900

    Title: 제곱하여 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250730/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,a;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        cout<<a*a<<' ';
+    }
+    return 0;
+}
