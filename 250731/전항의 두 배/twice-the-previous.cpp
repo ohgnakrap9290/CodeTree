23dcbd3bda9542cd7861b3f5d86c1c9cdcf0a085
ohgnakrap9290 <phantom4862@naver.com>
2025-07-31 01:11:55 +0900

    Title: 전항의 두 배, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250731/전항의 두 배/twice-the-previous.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int array[10];
+    cin>>array[0]>>array[1];
+    for(int i=2;i<10;i++){
+        array[i]=array[i-1]+2*array[i-2];
+    }
+    for(int i=0;i<10;i++){
+        cout<<array[i]<<' ';
+    }
+
+    return 0;
+}
