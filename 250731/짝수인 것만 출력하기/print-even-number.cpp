ee52c9a5a118eaaec6f73fb5268e24535b8fe0b5
ohgnakrap9290 <phantom4862@naver.com>
2025-07-31 00:53:37 +0900

    Title: 짝수인 것만 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250731/짝수인 것만 출력하기/print-even-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,a;
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+        cin>>a;
+        if(a%2==0){
+            cout<<a<<' ';
+        }
+    }
+    return 0;
+}
