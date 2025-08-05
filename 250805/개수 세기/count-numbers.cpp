26580e16916ca1bdeb9ffef5c83b08fb2ecf4a35
ohgnakrap9290 <phantom4862@naver.com>
2025-08-05 21:34:52 +0900

    Title: 개수 세기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250805/개수 세기/count-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,cnt=0,m,num;
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        cin>>num;
+        if(num==m){
+            cnt+=1;
+        }
+    }
+    cout<<m;
+    return 0;
+}
