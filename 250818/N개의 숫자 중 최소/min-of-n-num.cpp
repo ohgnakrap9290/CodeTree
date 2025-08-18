eb009114ca0b47c67d1d0d03f2e531d0abd69727
ohgnakrap9290 <phantom4862@naver.com>
2025-08-18 11:11:09 +0900

    Title: N개의 숫자 중 최소, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250818/N개의 숫자 중 최소/min-of-n-num.cpp
+++ 250818/N개의 숫자 중 최소/min-of-n-num.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int array[100],n,min=100,cnt=0;
+    int array[100],n,cnt=0;
     cin>>n;
     for(int i=0;i<n;i++){
         cin>>array[i];
     }
+    int min=array[0];
     for(int i=0;i<n;i++){
         if(array[i]<min){
             min=array[i];
