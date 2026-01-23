128a9bab2d81a5af4d6e20de283c36a2e9905882
k@NGh5! <phantom4862@naver.com>
2026-01-24 03:39:11 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260124/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int price[1000];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> price[i];
+    }
+    int max=0;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(price[j]-price[i]>max){
+                max = price[j]-price[i];
+            }
+        }
+    }
+    cout<<max;
+
+    // Please write your code here.
+
+    return 0;
+}
